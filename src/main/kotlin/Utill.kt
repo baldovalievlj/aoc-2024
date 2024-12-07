0405bc6b46024a8ceb541077be03b2f2b5e199b7
Ljupche <baldovaliev.b@gmail.com>
2024-12-07 11:50:30 +0100

    day 7

--- src/main/kotlin/Utill.kt
+++ src/main/kotlin/Utill.kt
@@ -2,4 +2,8 @@ import java.io.File
 
 fun getResourceFile(fileName: String): File = File(ClassLoader.getSystemResource("${fileName}.txt").file)
 
-fun List<String>.splitToInts(delimiter: String = " "): List<List<Int>> = this.map { it.split(delimiter).map { number -> number.toInt() } }
+fun List<String>.splitToInts(delimiter: String = " "): List<List<Int>> = this.map { it.splitToInts(delimiter) }
+
+fun String.splitToInts(delimiter: String = " "): List<Int> = this.split(delimiter).map { it.trim().toInt() }
+
+fun String.splitToLongs(delimiter: String = " "): List<Long> = this.split(delimiter).map { it.trim().toLong() }
---
+++ src/main/kotlin/day7/BridgeRepair.kt
@@ -0,0 +1,54 @@
+package day7
+
+import getResourceFile
+import splitToLongs
+
+fun main() {
+    val lines = getResourceFile("day7").readLines()
+
+    println(mapLines(lines).getSumsOfEquation())
+
+    println(mapLines(lines).getSumsOfEquationWithConcatenation())
+}
+
+fun Map<Long, List<Long>>.getSumsOfEquation(): Long = this.getSumsPossibleWith{
+    findCombinations(0, 0)
+}
+
+fun Map<Long, List<Long>>.getSumsOfEquationWithConcatenation(): Long = this.getSumsPossibleWith{
+    findCombinationsWithConcatenation(0, 0)
+}
+
+fun Map<Long, List<Long>>.getSumsPossibleWith(block: List<Long>.() -> List<Long>): Long =
+    this.keys.fold(0) { acc, key ->
+        this[key]?.let { values ->
+            values.block().firstOrNull { it == key }
+        }?.let { acc + it } ?: acc
+    }
+
+tailrec fun List<Long>.findCombinations(currentIndex: Int, value: Long): List<Long> {
+    val current = this[currentIndex]
+    val multiplier = value.takeIf { it != 0L } ?: 1
+    if (currentIndex == this.size - 1) {
+        return listOf(current + value, current * multiplier)
+    }
+    return this.findCombinations(currentIndex + 1, current + value) +
+            this.findCombinations(currentIndex + 1, current * multiplier)
+}
+
+tailrec fun List<Long>.findCombinationsWithConcatenation(currentIndex: Int, value: Long): List<Long> {
+    val current = this[currentIndex]
+    val multiplier = value.takeIf { it != 0L } ?: 1
+    if (currentIndex == this.size - 1) {
+        return listOf(current + value, current * multiplier, "${value}${current}".toLong())
+    }
+    return this.findCombinationsWithConcatenation(currentIndex + 1, current + value) +
+            this.findCombinationsWithConcatenation(currentIndex + 1, current * multiplier) +
+            this.findCombinationsWithConcatenation(currentIndex + 1, "${value}${current}".toLong())
+}
+
+
+fun mapLines(lines: List<String>): Map<Long, List<Long>> = lines.map {
+    val (number, numbers) = it.split(": ")
+    number.toLong() to numbers.splitToLongs()
+}.toMap()
